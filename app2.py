aa59c97b61c78d14206d2bbc671e36cb69fa53d2
Mishka Agarwal <agarwalmishka6@gmail.com>
2025-11-08 12:00:32 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,3 +4,6 @@ with st.chat_message("user"):
     st.write("Hello 👋")
 with st.chat_message("assistant"):
     st.write("Hello human")
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
